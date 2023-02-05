ab0fcd238be996ce214c22af737a6635fcd7647c
Stephen Amaza <steveamaza@gmail.com>
2023-02-05 20:57:05 +0000

    nodemon to automatically refresh servers

--- myApp.js
+++ myApp.js
@@ -1,8 +1,13 @@
 let express = require('express');
 let app = express();
 
+console.log("Hello World");
 
+app.get('/', (req, res) => {
+    res.sendFile (__dirname + "/views/index.html");
+});
 
+app.use('/public', express.static('public'));
 
 
 
