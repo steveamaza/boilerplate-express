3b08ed7f8e731da5dab0a4f2985153f9a2585397
Stephen Amaza <steveamaza@gmail.com>
2023-02-10 10:14:14 +0000

    Practice chaining middleware

--- myApp.js
+++ myApp.js
@@ -1,7 +1,7 @@
+require('dotenv').config();
+
 let express = require('express');
 let app = express();
-
-console.log("Hello World");
 
 app.get('/', (req, res) => {
     res.sendFile (__dirname + "/views/index.html");
@@ -10,38 +10,29 @@ app.get('/', (req, res) => {
 app.use('/public', express.static('public'));
 
 app.get('/json', (req, res) => {
-    res.json ({"message": "Hello json"});
+    const myEnv = process.env.MESSAGE_STYLE;
+
+    if (myEnv == "uppercase"){
+        message = {"message":"HELLO JSON"};
+    } else {
+        message = {"message":"Hello json"};
+    }
+    res.json (message);
 });
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.path} - ${req.ip}`);
+    next();
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.get('/now', (req, res, next) => {
+    // Middleware function 1
+    console.log('Middleware function 1');
+    next();
+}, function(req, res){
+    req.time = new Date().toString();
+    console.log(req.time);
+    res.json({time: req.time});
+});
+  
  module.exports = app;
